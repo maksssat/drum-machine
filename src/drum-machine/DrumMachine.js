6d8a837c0b43f72f125f9685ad1b31ec4504d236
maksat_sagandykov <maksssat@gmail.com>
2021-01-30 01:19:37 +0600

    стилизация и логика компонентов

--- src/drum-machine/DrumMachine.js
+++ src/drum-machine/DrumMachine.js
@@ -1,45 +1,127 @@
+import React from "react";
 import DrumPad from "./DrumPad";
 
-export default function DrumMachine(props) {
-  return (
-    <div id="drum-machine">
-      <div id="display"></div>
+const samples = [
+  {
+    value: "Q",
+    id: "Clap",
+    keyCode: 81,
+    src:
+      "https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Clap-1.mp3",
+  },
+  {
+    value: "W",
+    id: "Crash",
+    keyCode: 87,
+    src:
+      "https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Crash-HD2.mp3",
+  },
+  {
+    value: "E",
+    id: "HiHat",
+    keyCode: 69,
+    src:
+      "https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-HiHatOpen-D6.mp3",
+  },
+  {
+    value: "A",
+    id: "HiTom",
+    keyCode: 65,
+    src:
+      "https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-HiTom-3D7.mp3",
+  },
+  {
+    value: "S",
+    id: "Kick",
+    keyCode: 83,
+    src:
+      "https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Kick-T3A0DA.mp3",
+  },
+  {
+    value: "D",
+    id: "LoTom",
+    keyCode: 68,
+    src:
+      "https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-LoTom-0D7.mp3",
+  },
+  {
+    value: "Z",
+    id: "MidTom",
+    keyCode: 90,
+    src:
+      "https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-MidTom-0DA.mp3",
+  },
+  {
+    value: "X",
+    id: "Ride",
+    keyCode: 88,
+    src:
+      "https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Ride-D0.mp3",
+  },
+  {
+    value: "C",
+    id: "Snare",
+    keyCode: 67,
+    src:
+      "https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Snare-T3T0S7.mp3",
+  },
+];
+
+export default class DrumMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: "",
+      volume: "1.0",
+    };
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handlePlay(e) {
+    this.setState({ display: e });
+  }
+
+  handleChange(e) {
+    this.setState({ volume: e.target.value });
+  }
+
+  componentDidUpdate() {
+    if (this.state.display !== "") {
+      setTimeout(() => {
+        this.setState({ display: "" });
+      }, 1000);
+    }
+  }
+
+  render() {
+    const drumpadList = samples.map(({ value, id, keyCode, src }) => (
       <DrumPad
-        value="Q"
-        src="https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Clap-1.mp3"
+        id={id}
+        value={value}
+        keyCode={keyCode}
+        src={src}
+        key={value}
+        onPlay={this.handlePlay}
+        volume={this.state.volume}
       />
-      <DrumPad
-        value="W"
-        src="https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Crash-HD2.mp3"
-      />
-      <DrumPad
-        value="E"
-        src="https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-HiHatOpen-D6.mp3"
-      />
-      <DrumPad
-        value="A"
-        src="https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-HiTom-3D7.mp3"
-      />
-      <DrumPad
-        value="S"
-        src="https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Kick-T3A0DA.mp3"
-      />
-      <DrumPad
-        value="D"
-        src="https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-LoTom-0D7.mp3"
-      />
-      <DrumPad
-        value="Z"
-        src="https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-MidTom-0DA.mp3"
-      />
-      <DrumPad
-        value="X"
-        src="https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Ride-D0.mp3"
-      />
-      <DrumPad
-        value="C"
-        src="https://raw.githubusercontent.com/maksssat/drum-machine/master/src/drum-machine/sounds/909-Snare-T3T0S7.mp3"
-      />
-    </div>
-  );
+    ));
+
+    return (
+      <div id="drum-machine">
+        <div id="drumpad-wrapper">{drumpadList}</div>
+        <div id="control-wrapper">
+          <div id="display">{this.state.display}</div>
+          <input
+            type="range"
+            min="0"
+            max="1.0"
+            step="0.1"
+            value={this.state.volume}
+            onChange={this.handleChange}
+          />
+        </div>
+      </div>
+    );
+  }
 }
--- src/drum-machine/DrumPad.js
+++ src/drum-machine/DrumPad.js
@@ -1,16 +1,37 @@
-import { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 export default function DrumPad(props) {
   const audio = useRef(null);
+  const button = useRef(null);
 
-  function handleClick() {
+  function handleMouseDown(e) {
     audio.current.play();
+    props.onPlay(e.target.id);
   }
 
+  function handleKeyDown(e) {
+    if (e.keyCode === props.keyCode) {
+      audio.current.play();
+      props.onPlay(audio.current.dataset.id);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    audio.current.volume = props.volume;
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  });
+
   return (
-    <button className="drum-pad" onClick={handleClick}>
+    <button
+      ref={button}
+      id={props.id}
+      className="drum-pad"
+      onMouseDown={handleMouseDown}
+    >
       {props.value}
       <audio
+        data-id={props.id}
         ref={audio}
         id={props.value}
         className="clip"
